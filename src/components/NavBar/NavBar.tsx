08a05259fd8e8c99926e5dc5eef8a8c6dcc511d6
Christian Rudder <ringthebellpepper@gmail.com>
2023-08-24 17:13:38 -0400

    nav bar shell

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import Hamburger from 'hamburger-react';
+import {Spin as Hamburger} from 'hamburger-react';
+import { SlLocationPin, SlPhone } from 'react-icons/sl';
 import './NavBar.scss';
 
 function App() {
@@ -8,10 +9,54 @@ function App() {
 		<>
 			<div className="navbar-container">
 				<img src={'usacc-logo.svg'} className="app-logo" alt="logo" />
-				<Hamburger toggled={isOpen} toggle={setOpen} />
+				<div className={isOpen? 'nav-button active' : 'nav-button'}>
+					<Hamburger toggled={isOpen} toggle={setOpen} />
+				</div>
 			</div>
-			<div className="side-menu">
-            
+			<div className={isOpen? 'side-menu active' : 'side-menu'} >
+				<div className='links-side-menu'>
+					<div className='side-nav-links'>
+						<div>menu</div>
+						<div>booking</div>
+						<div>volunteering</div>
+						<div>membership</div>
+					</div>
+					<div className='side-divider'/>
+					<div className='side-nav-links'>
+						<div>about us</div>
+						<div>facebook</div>
+					</div>
+					<div className='side-contact-container'>
+						<div className='side-contact'>
+							<div className='side-cotact-logo'>
+								<SlLocationPin />
+							</div>
+							<div className='side-contact-link'>
+                                10 Dunbar Ave, 
+                                Dorchester Center, 
+                                Ma 02124
+							</div>
+						</div>
+						<div className='side-contact'>
+							<div className='side-cotact-logo'>
+								<SlPhone />
+							</div>
+							<div className='side-contact-link'>
+                            (617) 436-9550
+							</div>
+						</div>
+						<div className='side-footer'>
+							<div className='side-email'>
+                            someLongEmailName@gmail.com
+							</div>
+							<div className='side-footer-text'>
+                            Unity Sports and Cultural Association is a 501(c)4 organization in Boston
+								<br/>
+                            © 2023 Unity Sports and Cultural Club. All rights reserved
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</>
 	);
