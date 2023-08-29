3e2578885a3bb3f8aa3f74547ad773186efcd3d5
Christian Rudder <ringthebellpepper@gmail.com>
2023-08-28 21:35:38 -0400

    initial setup and cleanup

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,13 @@ import React, { ReactElement } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import About from '../../pages/About/About';
 import Booking from '../../pages/Booking/Booking';
-import Home from '../../pages/Home/Home';
+// import Home from '../../pages/Home/Home';
 import Membership from '../../pages/Membership/Membership';
 import Menu from '../../pages/Menu/Menu';
 import Volunteer from '../../pages/Volunteer/Volunteer';
 import IsMobileContext from '../../utils/isMobileContext';
 import useIsMobile from '../../utils/useIsMobile';
+import DesktopShell from '../DesktopShell/DesktopShell';
 import Footer from '../Footer';
 import NavBar from '../NavBar';
 import './App.scss';
@@ -19,7 +20,7 @@ function App(): ReactElement {
             <Router>
                 <NavBar />
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<DesktopShell title='Home'><div>hello</div></DesktopShell>} />
                     <Route path="/menu" element={<Menu />} />
                     <Route path="/volunteering" element={<Volunteer />} />
                     <Route path="/about" element={<About />} />
