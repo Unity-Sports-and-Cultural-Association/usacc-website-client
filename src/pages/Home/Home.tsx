d7d2f85f19ce177431b769ad1d05dc60735e923a
SPC-Toad <sangyunkiment@gmail.com>
2023-08-28 23:04:12 -0400

    implemented home header

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,10 @@
 import React, { ReactElement } from 'react';
 import './Home.scss';
-import Header from '../../components/Header/Header';
+import HomeHeader from '../../components/HomeHeader/HomeHeader';
 function Home(): ReactElement {
     return (
         <div className='test-height'>
-            <Header
-                title='Renting a&#13;&#10;Service'
-                topic='Booking Halls, Bartenders, & Catering'
-                description='Unity provides services for booking If you are looking for a space to host your next event or authentic Caribbean catering.'
-            />
+            <HomeHeader />
         </div>
     );
 }
