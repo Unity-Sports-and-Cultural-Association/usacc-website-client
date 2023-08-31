6c39e95bc64237417cd3afc994dcb41e5e97a478
SPC-Toad <sangyunkiment@gmail.com>
2023-08-31 18:15:29 -0400

    implemented email UI and basic functions

--- src/components/HomeLearnMore/HomeLearnMore.tsx
+++ src/components/HomeLearnMore/HomeLearnMore.tsx
@@ -4,13 +4,15 @@ import { SlArrowRight } from 'react-icons/sl';
 type HomeLearnMore = {image: string, title: string, topic: string, description: string, button: string};
 function HomeLearnMore({ image, title, topic, description, button }: HomeLearnMore): ReactElement {
     return (
-        <div className='home-learn-more-container'>
-            <img src={image} className='home-learn-more-image-container'/>
-            <div className='home-learn-more-bottom-container'>
-                <div className='home-learn-more-title-container'>{title}</div>
-                <div className='home-learn-more-topic-container'>{topic}</div>
-                <div className='home-learn-more-description-container'>{description}</div>
-                <a href={button} className='home-learn-more-buttom-container'>Learn More<SlArrowRight className='home-learn-more-right-arrow-container' /></a>
+        <div className='home-learn-more-main-container'>
+            <div className='home-learn-more-container'>
+                <img src={image} className='home-learn-more-image-container'/>
+                <div className='home-learn-more-bottom-container'>
+                    <div className='home-learn-more-title-container'>{title}</div>
+                    <div className='home-learn-more-topic-container'>{topic}</div>
+                    <div className='home-learn-more-description-container'>{description}</div>
+                    <a href={button} className='home-learn-more-buttom-container'>Learn More<SlArrowRight className='home-learn-more-right-arrow-container' /></a>
+                </div>
             </div>
         </div>
     );
