485d5816981db73c107ef6bd02ddcb545de175ee
Christian Rudder <ringthebellpepper@gmail.com>
2023-09-18 23:19:12 -0400

    all pages on mobile uptodate

--- src/components/FacebookWidget/FacebookWidget.tsx
+++ src/components/FacebookWidget/FacebookWidget.tsx
@@ -7,9 +7,9 @@ function FacebookWidget(): ReactElement {
         <div className='facebook-widget'>
             <Iframe
                 url="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FUnitySportsClub%2F&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-                width="340"
-                height="500"
-                styles={{ border: 'none', overflow: 'hidden', borderRadius: '12px', scrollbarWidth: '0' }}
+                width="325"
+                height="450"
+                styles={{ border: 'none', overflow: 'hidden', borderRadius: '12px', scrollbarWidth: '0' , marginLeft: '.4rem' }}
                 scrolling="no"
                 frameBorder={0}
                 allowFullScreen={false}
--- src/pages/Mobile/Home/Home.tsx
+++ src/pages/Mobile/Home/Home.tsx
@@ -13,30 +13,32 @@ function Home(): ReactElement {
         <>
             <HomeHeader />
             <HomeBooking />
-            <MobileContent>
-                <HomeLearnMore
-                    image={'./assets/volunteering.png'}
-                    title="Volunteering"
-                    topic="Community Events"
-                    description="Unity Sports and Cultural Club offers a range of events and activities that give back to the community. Including opportunities for community service."
-                    button='./volunteering'
-                />
-                <HomeLearnMore
-                    image={'./assets/events.png'}
-                    title="Events"
-                    topic="Come Together"
-                    description="Our club hosts a variety kinds of events, from arts and crafts to DJs on stage. Unity provides an open space where there’s always something for everyone here at Unity."
-                    button='./membership'
-                />
-                <HomeLearnMore
-                    image={'./assets/membership.png'}
-                    title="Membership"
-                    topic="Making a Difference"
-                    description="Unity is always open to spirited individuals who support the mission and vision of the organization. Even though we are a Caribbean club everyone is welcome."
-                    button='./membership'
-                />
-                <FacebookWidget />
-            </MobileContent>
+            <div className='home-content-container'>
+                <MobileContent>
+                    <HomeLearnMore
+                        image={'./assets/volunteering.png'}
+                        title="Volunteering"
+                        topic="Community Events"
+                        description="Unity Sports and Cultural Club offers a range of events and activities that give back to the community. Including opportunities for community service."
+                        button='./volunteering'
+                    />
+                    <HomeLearnMore
+                        image={'./assets/events.png'}
+                        title="Events"
+                        topic="Come Together"
+                        description="Our club hosts a variety kinds of events, from arts and crafts to DJs on stage. Unity provides an open space where there’s always something for everyone here at Unity."
+                        button='./membership'
+                    />
+                    <HomeLearnMore
+                        image={'./assets/membership.png'}
+                        title="Membership"
+                        topic="Making a Difference"
+                        description="Unity is always open to spirited individuals who support the mission and vision of the organization. Even though we are a Caribbean club everyone is welcome."
+                        button='./membership'
+                    />
+                    <FacebookWidget />
+                </MobileContent>
+            </div>
             <LocationWidget />
             <Email />
         </>
