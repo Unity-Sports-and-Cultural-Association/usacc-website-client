e26d67d8e248bd2d54ae7b43aa65e19cd49ab9db
SPC-Toad <sangyunkiment@gmail.com>
2024-01-09 16:14:43 -0500

    check point for desktop homepage

--- src/components/HomeHeader/HomeHeader.tsx
+++ src/components/HomeHeader/HomeHeader.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react';
 import './HomeHeader.scss';
 import { SlArrowRight, SlLocationPin, SlPhone } from 'react-icons/sl';
 import { Link } from 'react-router-dom';
+import { Parallax } from 'react-scroll-parallax';
 function HomeHeader(): ReactElement {
     return (
         <div className='home-header-guide'>
@@ -14,7 +15,7 @@ function HomeHeader(): ReactElement {
                             <SlLocationPin className='home-header-contact-pin-container'/>
                             <div className='home-header-contact-text-container'>
                                 <a href="https://www.google.com/maps/place/10+Dunbar+Ave,+Dorchester+Center,+Ma+02124" target="_blank" rel="noreferrer">
-                                10 Dunbar Ave,<br/> Dorchester Center,<br/> MA 02124</a></div>
+                                10 Dunbar Ave, Dorchester Center, MA 02124</a></div>
                         </div>
                         <div className='home-header-contact-list-container'>
                             <SlPhone className='home-header-contact-icon-container'/>
@@ -29,6 +30,17 @@ function HomeHeader(): ReactElement {
                     </div>
                 </div>
             </div>
+            <div className='home-renting-container'>
+                <div className='home-renting-title'>Renting a Service</div>
+                <div className='home-renting-subtitle'>Booking Halls, Bartenders, & Catering</div>
+                <div className='home-renting-desc'>Unity provides services for booking If you are looking for a space to host your next event or authentic Caribbean catering.</div>
+                <a href='./Booking' className='home-renting-button'>Request a Quote</a>
+            </div>
+            <Parallax speed={-30} className='home-header-parallax-container'>
+                <div className='home-header-parallax-title'>
+                    Unity
+                </div>
+            </Parallax>
         </div>
     );
 }
