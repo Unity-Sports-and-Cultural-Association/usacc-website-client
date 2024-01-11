2bd7fbcab84cdae83671cfff666372cc5a65cf74
Christian Rudder <74576449+neezacoto@users.noreply.github.com>
2024-01-11 17:45:03 -0500

    Meta data (#50)
    * trying to add netlify compatibility
    
    * possible email form fix
    
    * submitted interaction
    
    * email submission changes and styling
    
    * adding images for long lounge & styles
    
    * adding correct links and updating styles
    
    * link fixes
    
    * bottom nav link sytles
    
    * style fixing
    
    * event page and eventbrite fake widget, style overhall
    
    * big update
    
    * styling fixes
    
    * footer fix
    
    * fixing icon meta

--- src/components/VolunteeringDesktop/VolunteeringDesktop.tsx
+++ src/components/VolunteeringDesktop/VolunteeringDesktop.tsx
@@ -14,7 +14,7 @@ function VolunteeringDesktop(): ReactElement {
                         <a href='/volunteering' id='volunteering-button'>Learn More</a>
                     </div>
                 </div>
-                <img src='/assets/volunteer-upscale.png' alt='' className='volunteering-img'/>
+                <img src='/assets/volunteer-upscale.png' alt='volunteering-event' className='volunteering-img'/>
             </div>
         </div>
     );
